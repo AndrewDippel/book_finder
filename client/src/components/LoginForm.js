af486379c78d8a7311feb32bc1f7a427e8f0c30e
Andrew Dippel <a.t.dippel@gmail.com>
2023-01-04 16:17:26 +1100

    ensure all files up to date

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/react-hooks';
@@ -18,29 +17,14 @@ const LoginForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      });
+      Auth.login(data.login.token)
+    } catch (e) {
+      console.error(e);
     }
-
     setUserFormData({
       username: '',
       email: '',
